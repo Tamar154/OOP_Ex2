9a600a3d5d1830025416c262dd8cd8dd2cd95eaf
tamar-revazishvili <92332147+tamar-revazishvili@users.noreply.github.com>
2021-12-09 18:11:20 +0200

    Update DirectedWeightedGraphAlgorithmsImpl.java

--- Ex2/src/api/DirectedWeightedGraphAlgorithmsImpl.java
+++ Ex2/src/api/DirectedWeightedGraphAlgorithmsImpl.java
@@ -14,7 +14,7 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
     public DirectedWeightedGraphImpl g;
     final double INF = 99999.0;
 
-    public DirectedWeightedGraphAlgorithmsImpl() { // **check**
+    public DirectedWeightedGraphAlgorithmsImpl() {
         g = new DirectedWeightedGraphImpl();
     }
 
@@ -29,9 +29,6 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
         return g;
     }
 
-    /*
-    deep copy
-     */
     @Override
     public DirectedWeightedGraph copy() {
         DirectedWeightedGraphImpl ans = new DirectedWeightedGraphImpl();
@@ -85,14 +82,6 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
         }
     }
 
-    /**
-     * Computes the length of the shortest path between src to dest
-     * Note: if no such path --> returns -1
-     *
-     * @param src  - start node
-     * @param dest - end (target) node
-     * @return
-     */
     @Override
     public double shortestPathDist(int src, int dest) {
         double[][] mat = floydWarshallAlgorithm();
@@ -100,17 +89,6 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
             return mat[src][dest];
         return -1;
     }
-
-    /**
-     * Computes the the shortest path between src to dest - as an ordered List of nodes:
-     * src--> n1-->n2-->...dest
-     * see: https://en.wikipedia.org/wiki/Shortest_path_problem
-     * Note if no such path --> returns null;
-     *
-     * @param src  - start node
-     * @param dest - end (target) node
-     * @return
-     */
 
     @Override
     public List<NodeData> shortestPath(int src, int dest) {
@@ -120,7 +98,8 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
         int[][] next = new int[n][n];
         double[][] mat = new double[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {                if (i == j)
+            for (int j = 0; j < n; j++) {
+                if (i == j)
                     mat[i][j] = 0;
                 else
                     mat[i][j] = INF;
@@ -142,7 +121,7 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
             }
         }
 
-//        floyd:
+//        Floyd:
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -163,125 +142,27 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
         }
         return ans;
     }
-//    @Override
-//    public List<NodeData> shortestPath(int src, int dest) {
-//        List<NodeData> ans = new LinkedList<>();
-//        NodeData start = g.Nodes.get(src);
-//        NodeData end = g.Nodes.get(dest);
-//
-//        HashMap<NodeData, NodeData> changedAt = new HashMap<>();
-//        changedAt.put(start, null);
-//
-//        HashMap<NodeData, Double> shortestPathMap = new HashMap<>();
-////        HashMap<Integer, HashMap<NodeData,Double>> shortestPathMap = new HashMap<>();
-//
-//        HashMap<Integer, Boolean> visited = new HashMap<>();
-//        Iterator<NodeData> nodeIter = g.nodeIter();
-//        while (nodeIter.hasNext()) {
-//            NodeData temp = nodeIter.next();
-//            visited.put(temp.getKey(), false);
-//        }
-//
-//
-//        for (NodeData node : g.Nodes.values()) {
-//            if (node.equals(start)) {
-//                ans.add(node);
-//                shortestPathMap.put(start, 0.0);
-//            } else
-//                shortestPathMap.put(node, Double.MAX_VALUE);
-//        }
-//
-//        Iterator<EdgeData> iter = g.edgeIter();
-//        while (iter.hasNext()) {
-//            EdgeData edge = iter.next();
-//            shortestPathMap.put(g.getNode(edge.getDest()), edge.getWeight());
-//            changedAt.put(g.getNode(edge.getDest()), start);
-//        }
-//
-//        visited.replace(start.getKey(), true);
-//
-//        while (true) {
-//            NodeData currentNode = closestReachableUnvisited(shortestPathMap, visited);
-//            if (currentNode == null) {
-//                return null;
-//            }
-//            if (currentNode.equals(end)) {
-////                ans.add(currentNode);
-//                NodeData child = end;
-//                ans.add(g.getNode(dest));
-//                while (true) {
-//                    NodeData parent = changedAt.get(child);
-//                    if (parent != null) {
-//                        ans.add(parent);
-//                        child = parent;
-//                    } else
-//                        return ans;
-//                }
-////                return ans;
-//            }
-//            visited.replace(currentNode.getKey(), true);
-//
-//            iter = g.edgeIter();
-//            while (iter.hasNext()) {
-//                EdgeData edge = iter.next();
-//                if (visited.get(edge.getDest()))
-//                    continue;
-//
-//                if (shortestPathMap.get(currentNode) + edge.getWeight() < shortestPathMap.get(g.getNode(edge.getDest()))) {
-//                    shortestPathMap.put(g.getNode(edge.getDest()), shortestPathMap.get(currentNode) + edge.getWeight());
-//                    changedAt.put(g.getNode(edge.getDest()), currentNode);
-//                }
-//            }
-//            break;
-//        }
-//        return ans;
-//    }
 
-    private NodeData closestReachableUnvisited(HashMap<NodeData, Double> shortestPathMap, HashMap<Integer, Boolean> visited) {
-        double shortestDistance = Double.MAX_VALUE;
-        NodeData closestReachableNode = null;
-        for (NodeData node : g.Nodes.values()) {
-            if (visited.get(node.getKey()))
-                continue;
-
-            double currentDistance = shortestPathMap.get(node);
-            if (currentDistance == Double.MAX_VALUE)
-                continue;
-
-            if (currentDistance < shortestDistance) {
-                shortestDistance = currentDistance;
-                closestReachableNode = node;
-            }
-        }
-        return closestReachableNode;
-    }
-
-    /**
-     * Finds the NodeData which minimizes the max distance to all the other nodes.
-     * Assuming the graph isConnected, elese return null. See: https://en.wikipedia.org/wiki/Graph_center
-     *
-     * @return the Node data to which the max shortest path to all the other nodes is minimized.
-     */
     @Override
     public NodeData center() {
         int n = g.Nodes.size();
         double[][] D = floydWarshallAlgorithm();
-        double[] vm = new double[n];
+        double[] temp = new double[n];
         for (int i = 0; i < n; i++) {
-            vm[i] = 0;
+            temp[i] = 0;
             for (int j = 0; j < n; j++) {
-                if (vm[i] < D[i][j]) {
-                    vm[i] = D[i][j];
+                if (temp[i] < D[i][j]) {
+                    temp[i] = D[i][j];
                 }
             }
         }
-        double minVm = INF;
+        double max = INF;
         for (int i = 0; i < n; i++) {
-            if (minVm > vm[i])
-                minVm = vm[i];
+            if (max > temp[i])
+                max = temp[i];
         }
         for (int i = 0; i < n; i++) {
-            if (vm[i] == minVm) {
+            if (temp[i] == max) {
                 return g.getNode(i);
             }
         }
@@ -291,7 +172,6 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
     private double[][] floydWarshallAlgorithm() {
         int n = g.nodeSize();
         double[][] ans = new double[n][n];
-//        int i, j, k;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == j)
@@ -305,7 +185,6 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
             EdgeData temp = iter.next();
             ans[temp.getSrc()][temp.getDest()] = temp.getWeight();
         }
-
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -316,53 +195,43 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
             }
         }
         return ans;
-
     }
 
-    /**
-     * Computes a list of consecutive nodes which go over all the nodes in cities.
-     * the sum of the weights of all the consecutive (pairs) of nodes (directed) is the "cost" of the solution -
-     * the lower the better.
-     * See: https://en.wikipedia.org/wiki/Travelling_salesman_problem
-     */
     @Override
     public List<NodeData> tsp(List<NodeData> cities) {
-        double[][] tsp = floydWarshallAlgorithm();
-//        int sum = 0;
+        double[][] mat = floydWarshallAlgorithm();
         int counter = 0;
         int j = 0, i = 0;
         double min = Double.MAX_VALUE;
-        List<NodeData> visitedRouteList = new ArrayList<>();
-        visitedRouteList.add(cities.get(0));
-        int[] route = new int[tsp.length];
-        while (i < tsp.length && j < tsp[i].length) {
-            if (counter >= tsp[i].length - 1) {
+        List<NodeData> visited = new ArrayList<>();
+        visited.add(cities.get(0));
+        int[] route = new int[mat.length];
+        while (i < mat.length && j < mat[i].length) {
+            if (counter >= mat[i].length - 1) {
                 break;
             }
-            if (j != i && !(visitedRouteList.contains(cities.get(j)))) {
-                if (tsp[i][j] < min) {
-                    min = tsp[i][j];
+            if (j != i && !(visited.contains(cities.get(j)))) {
+                if (mat[i][j] < min) {
+                    min = mat[i][j];
                     route[counter] = j + 1;
                 }
             }
             j++;
-            if (j == tsp[i].length) {
-//                sum += min;
+            if (j == mat[i].length) {
                 min = Integer.MAX_VALUE;
-                visitedRouteList.add(cities.get(route[counter] - 1));
+                visited.add(cities.get(route[counter] - 1));
                 j = 0;
                 i = route[counter] - 1;
                 counter++;
             }
         }
-        for (j = 0; j < tsp.length; j++) {
-            if ((i != j) && tsp[i][j] < min) {
-                min = tsp[i][j];
+        for (j = 0; j < mat.length; j++) {
+            if ((i != j) && mat[i][j] < min) {
+                min = mat[i][j];
                 route[counter] = j + 1;
             }
         }
-//        sum += min;
-        return visitedRouteList;
+        return visited;
     }
 
 
@@ -429,7 +298,6 @@ public class DirectedWeightedGraphAlgorithmsImpl implements DirectedWeightedGrap
             e.printStackTrace();
             return false;
         }
-
         return false;
     }
 }
