f4e3c4d22694bd93515a836fd764469b3cf3303f
tamar-revazishvili <92332147+tamar-revazishvili@users.noreply.github.com>
2021-12-13 21:42:39 +0200

    Update MyFrame.java

--- Ex2/src/MyFrame.java
+++ Ex2/src/MyFrame.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyFrame extends JFrame implements ActionListener, MouseListener {
     private DirectedWeightedGraphAlgorithms alg;
@@ -25,18 +27,15 @@ public class MyFrame extends JFrame implements ActionListener, MouseListener {
     private MenuItem isConnected;
     private MenuItem ShortestPathDist;
     private MenuItem ShortestPath;
-    private  Dimension screenSize;
-    private JLabel txt;
 
 
-    private void initPanel () {
-        JPanel mp = new MyPanel(alg,this.getWidth(),this.getHeight());
+    private void initPanel() {
+        JPanel mp = new MyPanel(alg, this.getWidth(), this.getHeight());
         mp.setVisible(true);
         mp.setSize(this.getPreferredSize());
         this.add(mp);
-
-
     }
+
     public MyFrame(DirectedWeightedGraphAlgorithms alg) {
         this.alg = alg;
         BuildFrame();
@@ -65,8 +64,8 @@ public class MyFrame extends JFrame implements ActionListener, MouseListener {
         menuBar.add((Edit));
         AddNode = new MenuItem(("Add node"));
         DeleteNode = new MenuItem(("Delete node"));
-        AddEdge= new MenuItem(("Add edge"));
-        DeleteEdge= new MenuItem(("Delete edge"));
+        AddEdge = new MenuItem(("Add edge"));
+        DeleteEdge = new MenuItem(("Delete edge"));
         Edit.add(AddNode);
         Edit.add(DeleteNode);
         Edit.add(AddEdge);
@@ -104,11 +103,9 @@ public class MyFrame extends JFrame implements ActionListener, MouseListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
         if (e.getSource() == center) {
             JOptionPane.showMessageDialog(this, alg.center().toString());
         }
-
         if (e.getSource() == isConnected) {
             Boolean b = alg.isConnected();
             JOptionPane.showMessageDialog(this, b);
@@ -127,7 +124,7 @@ public class MyFrame extends JFrame implements ActionListener, MouseListener {
         if (e.getSource() == Save) {
             String name = JOptionPane.showInputDialog(this, "Enter a file name to save as:");
             try {
-                alg.save(name + ".json");
+                alg.save("data\\" + name + ".json");
             } catch (Exception j) {
                 JOptionPane.showMessageDialog(this, "Operation failed! please try again.");
             }
@@ -157,66 +154,107 @@ public class MyFrame extends JFrame implements ActionListener, MouseListener {
             NodeData n = alg.getGraph().removeNode(remove);
             if (n == null) {
                 JOptionPane.showMessageDialog(this, "No such key! try again.");
-            }else
-            {
+            } else {
                 repaint();
-                    JOptionPane.showMessageDialog(this, "Operation Succeed!");
+                JOptionPane.showMessageDialog(this, "Operation Succeed!");
 
             }
         }
-        if(e.getSource()==AddEdge){
-            String Source=JOptionPane.showInputDialog(this,"Enter a source:");
-            String Destination=JOptionPane.showInputDialog(this,"Enter a destination:");
-            String Weight=JOptionPane.showInputDialog(this,"Enter a weight:");
-            int src=Integer.parseInt(Source);
-            int dst=Integer.parseInt(Destination);
-            int W=Integer.parseInt(Weight);
-            try{
+        if (e.getSource() == AddEdge) {
+            String Source = JOptionPane.showInputDialog(this, "Enter a source:");
+            String Destination = JOptionPane.showInputDialog(this, "Enter a destination:");
+            String Weight = JOptionPane.showInputDialog(this, "Enter a weight:");
+            int src = Integer.parseInt(Source);
+            int dst = Integer.parseInt(Destination);
+            int W = Integer.parseInt(Weight);
+            try {
                 alg.getGraph().connect(src, dst, W);
                 repaint();
                 JOptionPane.showMessageDialog(this, "Operation Succeed!");
-            }catch(Exception c){
+            } catch (Exception c) {
                 JOptionPane.showMessageDialog(this, "Operation failed! try again.");
             }
         }
-        if(e.getSource()==DeleteEdge){
-            String Source=JOptionPane.showInputDialog(this,"Enter a source:");
-            String Destination=JOptionPane.showInputDialog(this,"Enter a destination:");
-            int src=Integer.parseInt(Source);
-            int dst=Integer.parseInt(Destination);
-            try{
+        if (e.getSource() == DeleteEdge) {
+            String Source = JOptionPane.showInputDialog(this, "Enter a source:");
+            String Destination = JOptionPane.showInputDialog(this, "Enter a destination:");
+            int src = Integer.parseInt(Source);
+            int dst = Integer.parseInt(Destination);
+            try {
                 alg.getGraph().removeEdge(src, dst);
                 repaint();
                 JOptionPane.showMessageDialog(this, "Operation Succeed!");
-            }catch(Exception c){
+            } catch (Exception c) {
+                JOptionPane.showMessageDialog(this, "Operation failed! try again.");
+            }
+        }
+
+        if (e.getSource() == ShortestPathDist) {
+            String source = JOptionPane.showInputDialog(this, "Enter a source:");
+            String Destination = JOptionPane.showInputDialog(this, "Enter a destination:");
+            int src = Integer.parseInt(source);
+            int dst = Integer.parseInt(Destination);
+            try {
+                JOptionPane.showMessageDialog(this, alg.shortestPathDist(src, dst));
+                repaint();
+            } catch (Exception c) {
+                JOptionPane.showMessageDialog(this, "Operation failed! try again.");
+            }
+        }
+        if (e.getSource() == ShortestPath) {
+            String source = JOptionPane.showInputDialog(this, "Enter a source:");
+            String Destination = JOptionPane.showInputDialog(this, "Enter a destination:");
+            int src = Integer.parseInt(source);
+            int dst = Integer.parseInt(Destination);
+            try {
+                JOptionPane.showMessageDialog(this, alg.shortestPath(src, dst));
+                repaint();
+            } catch (Exception c) {
+                JOptionPane.showMessageDialog(this, "Operation failed! try again.");
+            }
+        }
+        if (e.getSource() == TSP) {
+            List<NodeData> cities = new ArrayList<>();
+            String str = "";
+            try {
+                while (!str.equals("x")) {
+                    str = JOptionPane.showInputDialog(this, "Enter node key or 'x' to terminate: ");
+                    if (!str.equals("x")) {
+                        int key = Integer.parseInt(str);
+                        cities.add(alg.getGraph().getNode(key));
+                    }
+                }
+                JOptionPane.showMessageDialog(this, alg.tsp(cities));
+            } catch (Exception c) {
                 JOptionPane.showMessageDialog(this, "Operation failed! try again.");
             }
         }
     }
-        @Override
-        public void mouseClicked (MouseEvent e){
 
-        }
+    @Override
+    public void mouseClicked(MouseEvent e) {
 
-        @Override
-        public void mousePressed (MouseEvent e){
-
-        }
-
-        @Override
-        public void mouseReleased (MouseEvent e){
-
-        }
-
-        @Override
-        public void mouseEntered (MouseEvent e){
-
-        }
-
-        @Override
-        public void mouseExited (MouseEvent e){
-
-        }
     }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
 
 
